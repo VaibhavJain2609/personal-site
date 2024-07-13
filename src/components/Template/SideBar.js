20140a3085444d6f21e2fa70fb2d91811ced7201
vaibhavjain2609 <vaibhavjain@protonmail.com>
2024-07-12 22:46:17 -0400

    fixed sidebar

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -19,11 +19,10 @@ const SideBar = () => (
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni,
-        and the VP of Engineering at <a href="https://smileidentity.com">Smile Identity</a>. Previously,
-        I was the co-founder and CTO of <a href="https://arthena.com">Arthena</a>
-        , co-founder of <a href="https://matroid.com">Matroid</a>, and worked at
-        {' '}<a href="https://planet.com">Planet</a> and <a href="https://facebook.com">Facebook</a>.
+      <p>Hi, I&apos;m Vaibhav. I am a <a href="https://csus.edu/">Sacramento State University</a> graduate with a B.Sc. in computer science.
+        Previously, I was an intern at <a href="https://hoonartek.com/">Hoonartek</a>,
+        web developer at <a href="https://kksociety.com">KK Society</a>, and an intern for the IT department
+        at <a href="https://woodstockschool.in">Woodstock School</a>.
       </p>
       <ul className="actions">
         <li>
